665d2cae46e9bf49338a145eeaa2f532ae5601b6
siyaram-0726 <154308917+siyaram-0726@users.noreply.github.com>
2024-01-16 13:40:39 +0530

    Add files via upload

---
+++ src/main/java/com/dairy/controller/MilkRateController.java
@@ -0,0 +1,25 @@
+package com.dairy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dairy.service.MilkRateService;
+
+@RestController
+@RequestMapping( "/milkRate" )
+public class MilkRateController {
+	
+	@Autowired
+	private MilkRateService milkRateService;
+	
+	@GetMapping( "/type/{type}/fat/{fat}/snf/{snf}/branchId/{branchId}" )
+	public ResponseEntity<Float> getMilkRateByFatAndSNF( @PathVariable String type, @PathVariable float fat, @PathVariable float snf, @PathVariable int branchId) {
+		return new ResponseEntity<>( milkRateService.getMilkRateByTypeAndFatAndSNFAndBranch(type,fat,snf,branchId), HttpStatus.OK );
+	}
+
+}
