24439b0cd2c0517a78bf265149401980eb713498
siyaram-0726 <154308917+siyaram-0726@users.noreply.github.com>
2024-01-16 13:47:42 +0530

    Add files via upload

---
+++ src/main/java/com/dairy/repository/MilkRateRepository.java
@@ -0,0 +1,14 @@
+package com.dairy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dairy.entity.Branch;
+import com.dairy.entity.MilkRate;
+
+@Repository
+public interface MilkRateRepository extends JpaRepository<MilkRate, Integer> {
+
+	MilkRate findByTypeAndFatAndSnfAndBranch(String type, float fat, float snf, Branch branch);
+
+}
